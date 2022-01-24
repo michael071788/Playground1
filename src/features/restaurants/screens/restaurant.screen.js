dbc8b242931e5c9772400c5f992fb98c6f9a7e4d
michael071788 <michael071788@gmail.com>
2022-01-24 22:02:55 +0400

    feat :tada: initial screen for restaurant

---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,13 @@
+import React, { useContext, useState } from "react";
+import { SafeAreaComponent } from "../../../components/utility/safearea.component";
+import { Text, View } from "react-native";
+
+export const RestaurantScreen = ({ navigation }) => {
+  return (
+    <SafeAreaComponent>
+      <View>
+        <Text>RestaurantScreen</Text>
+      </View>
+    </SafeAreaComponent>
+  );
+};
